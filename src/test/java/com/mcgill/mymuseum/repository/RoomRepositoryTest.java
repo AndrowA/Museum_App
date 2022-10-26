8d3d29b7500fbb22e1d60f3d06a57876a2a9ae63
AndrowAM <androw.abdelmalak@mail.mcgill.ca>
2022-10-26 14:09:11 -0400

    Wrote test case for the DisplayRoom

--- src/test/java/com/mcgill/mymuseum/repository/RoomRepositoryTest.java
+++ src/test/java/com/mcgill/mymuseum/repository/RoomRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.mcgill.mymuseum.repository;
 
+import com.mcgill.mymuseum.model.DisplayRoom;
+import com.mcgill.mymuseum.model.WorkDay;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,12 +24,20 @@ class RoomRepositoryTest {
     @Test
     public void testPersistAndLoadDisplayRoom() {
         //Create Objects
+        int roomCapacity = 100;
+        DisplayRoom displayRoom1 = new DisplayRoom();
+        displayRoom1.setRoomCapacity(roomCapacity);
 
         //Save Object
+        displayRoom1 = roomRepository.save(displayRoom1);
+        Long id = displayRoom1.getRoomId();
 
         //Read Object
+        displayRoom1 = (DisplayRoom) roomRepository.findById(id).get();
 
         //Assert that object has correct attributes
+        assertNotNull(displayRoom1);
+        assertEquals(roomCapacity,displayRoom1.getRoomCapacity());
     }
 
     @Test
