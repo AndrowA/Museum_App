db9bf571b477dcaf0276becd0aacf0263623a2bd
PetrescuRad <radu.sebas.petrescu@gmail.com>
2022-10-26 12:34:11 -0400

    accountRepo tests

--- src/test/java/com/mcgill/mymuseum/repository/AccountRepositoryTest.java
+++ src/test/java/com/mcgill/mymuseum/repository/AccountRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.mcgill.mymuseum.repository;
 
 import com.mcgill.mymuseum.model.Employee;
+import com.mcgill.mymuseum.model.President;
+import com.mcgill.mymuseum.model.Visitor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,7 @@ class AccountRepositoryTest {
     @Autowired
     private AccountRepository accountRepository;
 
-    @AfterEach
+   // @AfterEach
     public void clearDatabase() {
         accountRepository.deleteAll();
     }
@@ -42,23 +44,42 @@ class AccountRepositoryTest {
     @Test
     public void testPersistAndLoadVisitorAccount() {
         //Create Objects
+        String username = "radu.petrescu@mail.mcgill.ca";
+        String password = "password";
+        Visitor visitor1 = new Visitor();
+        visitor1.setEmail(username);
+        visitor1.setPassword(password);
 
         //Save Object
-
+        visitor1 = accountRepository.save(visitor1);
+        Long id = visitor1.getAccountId();
         //Read Object
-
+        visitor1 = (Visitor) accountRepository.findById(id).get();
         //Assert that object has correct attributes
+        assertNotNull(visitor1);
+        assertEquals(username,visitor1.getEmail());
+        assertEquals(password,visitor1.getPassword());
     }
 
     @Test
     public void testPersistAndLoadPresidentAccount() {
         //Create Objects
-
+        String username = "Joe_Biden@WhiteHouse.usa";
+        String password = "password";
+        President president = new President();
+        president.setEmail(username);
+        president.setPassword(password);
+        president.setHourlyWage(100);
+        president.setOvertimeHourlyWage(140);
         //Save Object
-
+        president = accountRepository.save(president);
+        Long id = president.getAccountId();
         //Read Object
-
+        president = (President) accountRepository.findById(id).get();
         //Assert that object has correct attributes
+        assertNotNull(president);
+        assertEquals(username,president.getEmail());
+        assertEquals(password,president.getPassword());
     }
 
 }
