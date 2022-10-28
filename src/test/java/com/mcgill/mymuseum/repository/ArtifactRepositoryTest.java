23e4e6203455dac66e6046f9719b869f970a9106
AndrowAM <androw.abdelmalak@mail.mcgill.ca>
2022-10-28 19:31:23 -0400

    Wrote test case for the Artifact.

--- src/test/java/com/mcgill/mymuseum/repository/ArtifactRepositoryTest.java
+++ src/test/java/com/mcgill/mymuseum/repository/ArtifactRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.mcgill.mymuseum.repository;
 
+import com.mcgill.mymuseum.model.Artifact;
+import com.mcgill.mymuseum.model.DisplayRoom;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +23,25 @@ class ArtifactRepositoryTest {
     @Test
     public void testPersistAndLoadArtifact() {
         //Create Objects
+        String artifactName = "Golden mask of Tut";
+        String artifactDescription = "the most famous and admired artifacts of ancient Egypt in history and the world. It is the funerary death mask of the Egyptian pharaoh King Tutankamun.";
+        String artifactPictureURL="www.maskOfTut.com";
+        Artifact artifact1 = new Artifact();
+        artifact1.setName(artifactName);
+        artifact1.setDescription(artifactDescription);
+        artifact1.setUrl(artifactPictureURL);
 
         //Save Object
+        artifact1 = artifactRepository.save(artifact1);
+        Long id = artifact1.getArtifactId();
 
         //Read Object
+        artifact1 = artifactRepository.findById(id).get();
 
         //Assert that object has correct attributes
+        assertNotNull(artifact1);
+        assertEquals(artifactName,artifact1.getName());
+        assertEquals(artifactDescription,artifact1.getDescription());
+        assertEquals(artifactPictureURL, artifact1.getUrl());
     }
 }
