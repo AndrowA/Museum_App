03e90d274bdbe3ed3bded5bccf5d388958830c5e
Yassine Meliani <idriss.meliani@gmail.com>
2022-11-18 14:00:02 -0500

    finished "/artifact/get/{id}"

--- src/main/java/com/mcgill/mymuseum/dto/ArtifactDTO.java
+++ src/main/java/com/mcgill/mymuseum/dto/ArtifactDTO.java
@@ -1,5 +1,22 @@
-package com.mcgill.mymuseum.DTO;
+package com.mcgill.mymuseum.dto;
+
+
+import com.mcgill.mymuseum.model.Artifact;
 
 public class ArtifactDTO {
+    public final Long id;
+    public final String name;
+    public final String description;
+    public final String url;
+    public final Long loanId;
+    public final Long roomId;
 
+    public ArtifactDTO(Artifact artifact){
+        this.id = artifact.getArtifactId();
+        this.name = artifact.getName();
+        this.description = artifact.getDescription();
+        this.url = artifact.getUrl();
+        this.loanId = artifact.getLoan().getLoanId();
+        this.roomId = artifact.getRoom().getRoomId();
+    }
 }
