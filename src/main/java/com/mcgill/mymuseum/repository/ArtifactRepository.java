c088c647163a3d2c9167d0abc352e2d78532dac8
PetrescuRad <radu.sebas.petrescu@gmail.com>
2022-11-18 15:19:13 -0500

    fix ArtifactRepository

--- src/main/java/com/mcgill/mymuseum/repository/ArtifactRepository.java
+++ src/main/java/com/mcgill/mymuseum/repository/ArtifactRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.awt.print.Pageable;
 
 public interface ArtifactRepository extends CrudRepository<Artifact,Long> {
-    Iterable<Artifact> findArtifactsByPageAndNumber(int amount, Pageable page);
-    Artifact modifyArtifact(int id, Artifact newArtifact);
 
 }
