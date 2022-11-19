31b37fe71e306ac40f0c3bffb1e590b8d6bd6d77
PetrescuRad <radu.sebas.petrescu@gmail.com>
2022-11-18 22:50:38 -0500

    added 3 new controller post methods for loan and their respective services

--- src/main/java/com/mcgill/mymuseum/controller/LoanController.java
+++ src/main/java/com/mcgill/mymuseum/controller/LoanController.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.sql.Date;
 import java.util.List;
@@ -31,59 +32,73 @@ public class LoanController {
     @Autowired
     ArtifactService artifactService;
 
+    @Transactional
     @GetMapping("/loan/{id}")
     public ResponseEntity getLoan(@PathVariable long id) throws MuseumException {
         try {
             Loan loan = loanService.retrieveLoanById(id);
-            LoanDTO loanDTO = new LoanDTO(loan.getStartDate(),loan.getEndDate(),loan.getLoanStatus(),loan.getLoanee().getEmail(),loan.getArtifact().getName(),loan.getArtifact().getArtifactId(),loan.getMyMuseum().getAddress());
+            LoanDTO loanDTO = new LoanDTO(loan.getStartDate(),loan.getEndDate(),loan.getLoanStatus(),loan.getLoanee().getEmail(),loan.getArtifact().getName(),loan.getArtifact().getArtifactId(),null);
             return new ResponseEntity<>(loanDTO, HttpStatus.OK);
         } catch (MuseumException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
+    @Transactional
     @PostMapping("/createLoanRequest/{visitorId}/{artifactId}")
-    public ResponseEntity createRequest(@RequestBody String body, @PathVariable Long artifactId, @PathVariable Long visitorId) throws MuseumException {
+    public ResponseEntity createRequest(@RequestBody String body,  @PathVariable(name="visitorId") Long visitorId, @PathVariable(name="artifactId") Long artifactId) throws MuseumException {
         ObjectMapper mapper = new ObjectMapper();
         try {
+            Loan loan1 = mapper.readValue(body, Loan.class);
             Visitor loanee = (Visitor) accountService.findAccountByID(visitorId);
             Artifact artifact = artifactService.retrieveArtifact(artifactId);
-            Loan loan = mapper.readValue(body, Loan.class);
-            loanee.addLoan(loan);
-            artifact.setLoan(loan);
-            loanService.saveLoan(loan);
-            LoanDTO loanDTO = new LoanDTO(loan.getStartDate(), loan.getEndDate(), loan.getLoanStatus(), loanee.getEmail(), artifact.getName(), artifactId, artifact.getMyMuseum().getAddress());
+            if (!loan1.setLoanee(loanee)){
+                throw new MuseumException("Loanee already has too many loans");
+            }
+            //loanee.addLoan(loan1);
+            if (!loan1.setArtifact(artifact)) {
+                throw new MuseumException("Artifact already has a loan");
+            }
+            //artifact.setLoan(loan1);
+            loanService.saveLoan(loan1);
+            LoanDTO loanDTO = new LoanDTO(loan1.getStartDate(), loan1.getEndDate(), loan1.getLoanStatus(), loan1.getLoanee().getEmail(), loan1.getArtifact().getName(), loan1.getArtifact().getArtifactId(),null);
             return new ResponseEntity<>(loanDTO, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-/*
+
+    @Transactional
     @PostMapping("/approveLoan/{id}")
-    public ResponseEntity createRequest() throws MuseumException {
+    public ResponseEntity approveLoan(@PathVariable(name="id") Long id) throws MuseumException {
         try {
-            return new ResponseEntity<>(, HttpStatus.OK);
-        } catch (MuseumException e) {
-            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
-    }
-    //unecessary?
-    @PostMapping("/modifyLoan/{id}")
-    public ResponseEntity createRequest() throws MuseumException {
-        try {
-            return new ResponseEntity<>(, HttpStatus.OK);
+            Loan loan = loanService.acceptLoan(id);
+            LoanDTO loanDTO = new LoanDTO(loan.getStartDate(), loan.getEndDate(), loan.getLoanStatus(), loan.getLoanee().getEmail(), loan.getArtifact().getName(), loan.getArtifact().getArtifactId(),null);
+            return new ResponseEntity<>(loanDTO, HttpStatus.OK);
         } catch (MuseumException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
-    @PostMapping("/returnLoan/{id}")
-    public ResponseEntity createRequest() throws MuseumException {
+      @PostMapping("/returnLoan/{id}")
+    public ResponseEntity returnLoan(@PathVariable(name="id") Long id) throws MuseumException {
         try {
-            return new ResponseEntity<>(, HttpStatus.OK);
+            Loan loan = loanService.returnLoan(id);
+            LoanDTO loanDTO = new LoanDTO(loan.getStartDate(), loan.getEndDate(), loan.getLoanStatus(), loan.getLoanee().getEmail(), loan.getArtifact().getName(), loan.getArtifact().getArtifactId(),null);
+            return new ResponseEntity<>(loanDTO, HttpStatus.OK);
         } catch (MuseumException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
- */
+
+    @PostMapping("/rejectLoan/{id}")
+    public ResponseEntity rejectLoan(@PathVariable(name="id") Long id) throws MuseumException {
+        try {
+            Loan loan = loanService.rejectLoan(id);
+            LoanDTO loanDTO = new LoanDTO(loan.getStartDate(), loan.getEndDate(), loan.getLoanStatus(), loan.getLoanee().getEmail(), loan.getArtifact().getName(), loan.getArtifact().getArtifactId(),null);
+            return new ResponseEntity<>(loanDTO, HttpStatus.OK);
+        } catch (MuseumException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/mcgill/mymuseum/service/LoanService.java
+++ src/main/java/com/mcgill/mymuseum/service/LoanService.java
@@ -13,6 +13,7 @@ import com.mcgill.mymuseum.repository.MyMuseumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.lang.reflect.AnnotatedType;
 import java.sql.Date;
 import java.util.Optional;
@@ -22,9 +23,9 @@ public class LoanService {
     @Autowired
     LoanRepository loanRepository;
     @Autowired
-    ArtifactRepository artifactRepository;
+    AccountService accountService;
     @Autowired
-    MyMuseumRepository museumRepository;
+    ArtifactService artifactService;
 
     public Iterable<Loan> getLoans() { return loanRepository.findAll(); }
 
@@ -38,15 +39,78 @@ public class LoanService {
 
     public Loan saveLoan(Loan loan){ return loanRepository.save(loan);}
 
+    @Transactional
+    public Loan createLoan(Loan loan, long visitorId, long artifactId) throws Exception {
+        Visitor loanee = (Visitor) accountService.findAccountByID(visitorId);
+        Artifact artifact = artifactService.retrieveArtifact(artifactId);
+        if (artifact == null){
+            throw new MuseumException("Could not retrieve artifact with id " + artifactId);
+        } else {
+            artifact.setLoan(loan);
+        }
+        if (loanee == null) {
+            throw new MuseumException("Could not retrieve visitor with id " + visitorId);
+        } else {
+            loanee.addLoan(loan);
+        }
+        loanRepository.save(loan);
+        return loan;
+    }
+
+    @Transactional
     public Loan acceptLoan(long id) throws MuseumException {
         if (loanRepository.findById(id).isPresent()) {
             if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Approved)) {
                 throw new MuseumException("Loan with id: "+id+" has already been approved.");
             } else if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Rejected)) {
                 throw new MuseumException("Loan with id: "+id+" has already been rejected.");
+            } else if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Available)) {
+                throw new MuseumException("Loan with id: " + id + " cannot be accepted since it wasn't requested yet.");
             } else {
                 //set status to accepted the loan
                 loanRepository.findById(id).get().setLoanStatus(Loan.LoanStatus.Approved);
+            }
+        } else {
+            throw new MuseumException("Loan with id: "+id+" does not exist.");
+        }
+        return loanRepository.findById(id).get();
+    }
+
+    @Transactional
+    public Loan rejectLoan(long id) throws MuseumException {
+        if (loanRepository.findById(id).isPresent()) {
+            if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Approved)) {
+                throw new MuseumException("Loan with id: " + id + " has already been approved.");
+            } else if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Rejected)) {
+                throw new MuseumException("Loan with id: " + id + " has already been rejected.");
+            } else if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Available)) {
+                throw new MuseumException("Loan with id: "+id+" has already been returned and is available.");
+            } else {
+                //set status to available the loan
+                loanRepository.findById(id).get().setLoanStatus(Loan.LoanStatus.Rejected);
+                //remove loan from loanee
+                loanRepository.findById(id).get().getLoanee().removeLoan(loanRepository.findById(id).get());
+            }
+        } else {
+            throw new MuseumException("Loan with id: "+id+" does not exist.");
+        }
+        return loanRepository.findById(id).get();
+    }
+
+    @Transactional
+    public Loan returnLoan(long id) throws MuseumException {
+        if (loanRepository.findById(id).isPresent()) {
+            if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.InReview)) {
+                throw new MuseumException("Loan with id: " + id + " is still in review.");
+            } else if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Rejected)) {
+                throw new MuseumException("Loan with id: " + id + " has already been rejected.");
+            } else if (loanRepository.findById(id).get().getLoanStatus().equals(Loan.LoanStatus.Available)) {
+                throw new MuseumException("Loan with id: "+id+" has already been returned and is available.");
+            } else {
+                //set status to available the loan
+                loanRepository.findById(id).get().setLoanStatus(Loan.LoanStatus.Available);
+                //remove loan from loanee
+                loanRepository.findById(id).get().getLoanee().removeLoan(loanRepository.findById(id).get());
             }
         } else {
             throw new MuseumException("Loan with id: "+id+" does not exist.");
