b91e45226cf9746c8b0382baeeb830932658ee0c
Yassine Meliani <idriss.meliani@gmail.com>
2022-11-19 14:29:08 -0500

    mockito unit test

---
+++ src/test/java/com/mcgill/mymuseum/service/ArtifactServiceTest.java
@@ -0,0 +1,110 @@
+package com.mcgill.mymuseum.service;
+
+import com.mcgill.mymuseum.model.Artifact;
+import com.mcgill.mymuseum.model.DisplayRoom;
+import com.mcgill.mymuseum.model.Loan;
+import com.mcgill.mymuseum.model.Room;
+import com.mcgill.mymuseum.repository.ArtifactRepository;
+import com.mcgill.mymuseum.repository.LoanRepository;
+import com.mcgill.mymuseum.repository.RoomRepository;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.swing.text.html.Option;
+import java.sql.Date;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ArtifactServiceTest {
+
+    @Mock
+    ArtifactRepository artifactRepository;
+
+    @InjectMocks
+    ArtifactService artifactService;
+
+    @Nested
+    @DisplayName("testing getAllArtifacts method")
+    class GetAllArtifacts{
+        @Test
+        void listIsNull() {
+            Mockito.when(artifactRepository.findAll()).thenReturn(Collections.emptyList());
+            assertEquals(Collections.emptyList(), artifactService.getAllArtifacts());
+            Mockito.verify(artifactRepository).findAll();
+        }
+
+        @Test
+        void listIsFull() {
+            List<Artifact> listOfArtifacts = new ArrayList<Artifact>();
+            Mockito.when(artifactRepository.findAll()).thenReturn(listOfArtifacts);
+            assertEquals(listOfArtifacts, artifactService.getAllArtifacts());
+            Mockito.verify(artifactRepository).findAll();
+        }
+    }
+
+
+
+    @Nested
+    @DisplayName("testing retrieveArtifact method")
+    class RetrieveArtifact{
+
+        @Test
+        @DisplayName("should throw exception when id does not exist")
+        void shouldThrowException_whenIdDoesntExist() {
+            long id = 5l;
+            Mockito.when(artifactRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+            assertThrows(Exception.class , ()->artifactService.retrieveArtifact(id));
+            Mockito.verify(artifactRepository).findById(id);
+        }
+
+        @Test
+        @DisplayName("should return the artifact with that id")
+        void testRetrieveArtifact_HappyPath(){
+            long id = 5l;
+            try {
+                Artifact artifact = new Artifact();
+                Mockito.when(artifactRepository.findById(id)).thenReturn(Optional.of(artifact));
+
+                assertEquals(artifact, artifactService.retrieveArtifact(id));
+                Mockito.verify(artifactRepository).findById(5l);
+            }catch(Exception e){
+                fail();
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("testing saveArtifact method")
+    class SaveArtifact{
+        @Test
+        @DisplayName("trying to save a null artifact")
+        void saveEmptyArtifact(){
+            Artifact artifact = new Artifact();
+            assertThrows(Exception.class, ()-> artifactService.saveArtifact(artifact));
+        }
+
+        @Test
+        @DisplayName("saving artifact successfully")
+        void saveArtifact(){
+            Artifact artifact = new Artifact();
+            artifact.setName("hello");artifact.setDescription("hello");artifact.setUrl("hello");
+            Mockito.when(artifactRepository.save(artifact)).thenReturn(artifact);
+            try {
+                assertEquals(artifact, artifactService.saveArtifact(artifact));
+                Mockito.verify(artifactRepository).save(artifact);
+            }catch(Exception e){
+                fail();
+            }
+        }
+    }
+
+}
