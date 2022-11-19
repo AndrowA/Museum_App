2e2f90fde7857fda847dcd732e8e19a343317865
Yassine Meliani <idriss.meliani@gmail.com>
2022-11-19 14:32:26 -0500

    artifact controller

---
+++ src/test/java/com/mcgill/mymuseum/controller/ArtifactControllerTest.java
@@ -0,0 +1,42 @@
+package com.mcgill.mymuseum.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtifactControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getArtifact() {
+    }
+
+    @Test
+    void getManyArtifacts() {
+    }
+
+    @Test
+    void addArtifact() {
+    }
+
+    @Test
+    void modifyArtifact() {
+    }
+
+    @Test
+    void assignRoom() {
+    }
+
+    @Test
+    void getRoom() {
+    }
+}
