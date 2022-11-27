2883ceec830a7fded58859e53233737c41a24336
Shady Guindi <shadyguindi13@gmail.com>
2022-11-27 18:12:18 -0500

    create login & sign up page with login/logout logic

--- Client/src/routes.js
+++ Client/src/routes.js
@@ -7,6 +7,7 @@ import DashboardLayout from './layouts/dashboard';
 import BlogPage from './pages/BlogPage';
 import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 import Page404 from './pages/Page404';
 import ProductsPage from './pages/ProductsPage';
 import DashboardAppPage from './pages/DashboardAppPage';
@@ -41,13 +42,11 @@ export default function Router() {
       : {
           path: 'login',
           element: <LoginPage />,
-          children: [
-            {
-              path: '*',
-              element: <Navigate to="login" />,
-            },
-          ],
         },
+    !isLoggedIn && {
+      path: 'register',
+      element: <SignUpPage />,
+    },
   ]);
 
   return routes;
