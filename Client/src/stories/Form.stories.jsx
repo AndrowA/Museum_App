40afbb566335187ecbb6bce90fe88a50cfd20a7a
Shady Guindi <shadyguindi13@gmail.com>
2022-11-28 17:50:53 -0500

    create unified form

--- Client/src/stories/Form.stories.jsx
+++ Client/src/stories/Form.stories.jsx
@@ -16,4 +16,53 @@ export default {
   ],
 };
 
-export const Primary = () => <Form />;
+export const LoanRequestForm = () => (
+  <Form fields={[]} datePickers={[{ title: 'loan start date' }]} buttons={[{ title: 'Request' }]} />
+);
+
+export const AddArtifactForm = () => {
+  const onChangeTitle = (e) => {
+    // value comes from e.target.value
+  };
+
+  const onButtonClick = (e) => {
+    alert('button clicked');
+  };
+
+  return (
+    <Form
+      fields={[
+        { title: 'Artifact Title', onChange: onChangeTitle },
+        { title: 'Artifact URL' },
+        { title: 'Artifact Description', desc: true },
+      ]}
+      buttons={[{ title: 'Add Artifact', onClick: onButtonClick }]}
+    />
+  );
+};
+
+export const AddEmployeeForm = () => {
+  const onChangeEmail = (e) => {
+    // value comes from e.target.value
+  };
+
+  const onButtonClick = (e) => {
+    alert('button clicked');
+  };
+
+  return (
+    <Form
+      fields={[
+        { title: 'Email', onChange: onChangeEmail },
+        { title: 'Password' },
+        { title: 'Base Hourly Salary ($)', number: true },
+        { title: 'Overtime Hourly Salary ($)', number: true },
+      ]}
+      buttons={[{ title: 'Add Employee', onClick: onButtonClick }]}
+    />
+  );
+};
+
+export const PassPurchaseForm = () => (
+  <Form fields={[]} datePickers={[{ title: 'Pass Date', dateOnly: true }]} buttons={[{ title: 'Request' }]} />
+);
